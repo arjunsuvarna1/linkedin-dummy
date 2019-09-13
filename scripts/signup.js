e087d07d9f206365a5fb0b44ffdede1526e35afe
arjunsuvarna1 <b8ibmjava11@iiht.tech>
2019-09-13 09:34:33 +0530

    linked script

--- scripts/signup.js
+++ scripts/signup.js
@@ -5,8 +5,9 @@ function signup(){
  var email =  document.getElementById('email').value;
  var password = document.getElementById('password').value;
 
- localStorage.setItem('FirstName', firstName);
+ localStorage.setItem('firstName', firstName);
  localStorage.setItem('lastName', lastName);
  localStorage.setItem('email', email);
  localStorage.setItem('password', password);
+ console.log(localStorage.getItem('firstName'),'yay');
 }
