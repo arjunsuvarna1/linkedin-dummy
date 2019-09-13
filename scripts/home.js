998b03aec7957efcae084f49ee55dc0c63778ac1
arjunsuvarna1 <b8ibmjava11@iiht.tech>
2019-09-13 11:34:27 +0530

    minor change

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,3 @@
 var name = sessionStorage.getItem('name');
 var serial = sessionStorage.getItem('serial');
-document.getElementById("name").innerHTML = "Hello "+name +localStorage.getItem(name+'lastName'); 
+document.getElementById("name").innerHTML = "Hello "+name+" " +localStorage.getItem(name+'lastName'); 
