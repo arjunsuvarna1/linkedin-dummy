6cccf528ab9453af58ffc75b7fe610690dd4f1fe
arjunsuvarna1 <b8ibmjava11@iiht.tech>
2019-09-13 09:48:33 +0530

    css

--- scripts/signin.js
+++ scripts/signin.js
@@ -2,4 +2,9 @@
 function signin(){
     var email =  document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    if(email==localStorage.getItem('email')){
+        if(password==localStorage.getItem('password')){
+            window.location('/home.html');
+        }
+    }
 }
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,13 +1,12 @@
 /*Sign Up Functionality */
 function signup(){                                                      
  var firstName =   document.getElementById('firstName').value;
- var lastName = document.getElementById('LastName').value;
+ var lastName = document.getElementById('lastName').value;
  var email =  document.getElementById('email').value;
  var password = document.getElementById('password').value;
 
  localStorage.setItem('firstName', firstName);
  localStorage.setItem('lastName', lastName);
- localStorage.setItem('email', email);
- localStorage.setItem('password', password);
- console.log(localStorage.getItem('firstName'),'yay');
+ localStorage.setItem(firstName+'email', email);
+ localStorage.setItem(last+'password', password);
 }
