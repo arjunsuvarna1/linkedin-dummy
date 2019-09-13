2a40efbeb03633d3c8524e768fb0377d2f14d4a0
arjunsuvarna1 <b8ibmjava11@iiht.tech>
2019-09-13 12:15:35 +0530

    homepage

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,4 @@
+/* Display name of user on homepage*/
 var name = sessionStorage.getItem('name');
 var serial = sessionStorage.getItem('serial');
-document.getElementById("name").innerHTML = "Hello "+name+" " +localStorage.getItem(name+'lastName'); 
+document.getElementById("name").innerHTML = "<h2> Hello "+name+" " +localStorage.getItem(name+'lastName')+ "</h2>"; 
